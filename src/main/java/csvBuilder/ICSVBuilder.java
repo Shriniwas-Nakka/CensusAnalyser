d7fd432e0fb5e30186fa847d47763ae759e31eee
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-12-10 11:01:27 +0530

    REEFACTORED: code to introduced DAO accomdate encapsulation

--- src/main/java/csvBuilder/ICSVBuilder.java
+++ src/main/java/csvBuilder/ICSVBuilder.java
@@ -7,6 +7,5 @@ import java.util.List;
 public interface ICSVBuilder<E> {
 
     public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
-
     public List<E> getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException;
 }
